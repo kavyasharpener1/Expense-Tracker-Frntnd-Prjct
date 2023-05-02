e63dbb578324c3a2bf48f29a53b8267189c9b643
kavyasharpener1 <naikkavya201@gmail.com>
2023-05-02 17:39:38 +0530

    completed expense tracker

---
+++ src/components/StartingPage/StartingPageContent.js
@@ -0,0 +1,12 @@
+import classes from './StartingPageContent.module.css';
+
+const StartingPageContent = () => {
+  return (
+    <section className={classes.starting}>
+      <h1> WELCOME TO EXPENSE TRACKER </h1>
+      <h2> (TRACK MY EXPENSES) </h2>
+    </section>
+  );
+};
+
+export default StartingPageContent;
